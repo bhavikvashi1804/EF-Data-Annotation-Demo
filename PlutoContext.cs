f30aad39520cf55754c7a2e74330b3b5e84c691b
Bhavik Vashi <bhavikvashi1804@gmail.com>
2020-11-01 11:14:32 +0530

    Add the Context class

---
+++ PlutoContext.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    public class PlutoContext : DbContext
+    {
+        //to get the list of course
+        public DbSet<Course> Courses { get; set; }
+        //to get the list of Authors
+        public DbSet<Author> Authors { get; set; }
+        //to get the list of Tags
+        public DbSet<Tag> Tags { get; set; }
+
+        public PlutoContext()
+            : base("name=DefaultConnection")
+        {
+
+        }
+    }
+}
