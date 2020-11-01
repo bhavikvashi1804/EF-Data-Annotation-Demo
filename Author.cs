8365c691d10e922cce544079299f38d4a9a28861
Bhavik Vashi <bhavikvashi1804@gmail.com>
2020-11-01 10:56:08 +0530

    Add project files.

---
+++ Author.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    public class Author
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public IList<Course> Courses { get; set; }
+        //Author can have many courses
+        // Course - Author is one to many relationship
+    }
+}
